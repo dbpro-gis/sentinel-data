cf5a106684a168cc005d11b5768a749022d613ce
Max Zhao <alcasa.mz@gmail.com>
2019-05-23 14:59:28 +0200

    Adding dockerfile for running the downloader

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.7-stretch
+
+RUN pip install requests requests_cache pyshp shapely matplotlib
+
+WORKDIR /app
+COPY ./fetch_images.py ./
+CMD python3 fetch_images.py
---
+++ run_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+case $1 in
+build)
+	docker build -t sentinel-data .
+	;;
+*)
+	docker run --name sentinel-data --env COPERNICUS_USER=$COPERNICUS_USER --env COPERNICUS_PASS=$COPERNICUS_PASS --rm sentinel-data
+	;;
+esac

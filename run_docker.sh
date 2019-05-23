8e0f88f8bfa5dbc74851c3ae3c0500ad12ea6013
Max Zhao <alcasa.mz@gmail.com>
2019-05-23 18:00:00 +0000

    Adding finished filepaths

--- run_docker.sh
+++ run_docker.sh
@@ -4,6 +4,6 @@ build)
 	docker build -t sentinel-data .
 	;;
 *)
-	docker run --name sentinel-data -v $(pwd):/app --env COPERNICUS_USER=$COPERNICUS_USER --env COPERNICUS_PASS=$COPERNICUS_PASS --rm sentinel-data
+	docker run --name sentinel-data -v $(pwd):/app -u $(id -u):$(id -g) --env COPERNICUS_USER=$COPERNICUS_USER --env COPERNICUS_PASS=$COPERNICUS_PASS --rm sentinel-data
 	;;
 esac

aa5a57be1adf4c84cc6574b52d2fcf8981dd2e88
Max Zhao <alcasa.mz@gmail.com>
2019-05-26 21:16:22 +0000

    Adding tif conversion script

--- download_files.sh
+++ download_files.sh
@@ -4,5 +4,5 @@ outdir=download
 mkdir -p $outdir
 while read p; do
 	filename=$(echo $p | grep -o '[[:alnum:]_]*.jp2')
-	curl -v -o "$outdir/$filename" -u $COPERNICUS_USER:$COPERNICUS_PASS "https://scihub.copernicus.eu/dhus/odata/v1$p"
+	curl -o "$outdir/$filename" -u $COPERNICUS_USER:$COPERNICUS_PASS "https://scihub.copernicus.eu/dhus/odata/v1$p"
 done <$1

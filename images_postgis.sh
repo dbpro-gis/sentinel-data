def77c78ddfde22397c05c13061c85706278ae64
Max Zhao <alcasa.mz@gmail.com>
2019-06-01 15:00:18 +0200

    Fixing image import

--- images_postgis.sh
+++ images_postgis.sh
@@ -4,5 +4,5 @@ inputdir="download-tif"
 
 for filename in $inputdir/*.tif; do
 	echo $filename
-	raster2pgsql -s 32633 -I -t 500x500 -C $filename | psql -U postgres -h home.arsbrevis.de -p 31313 -d $DBNAME >> postgis_import.log
+	raster2pgsql -I -t 120x120 -C $filename | psql -U postgres -h home.arsbrevis.de -p 31313 -d $DBNAME >> postgis_import.log
 done

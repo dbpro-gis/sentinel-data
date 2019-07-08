9bd98bb43cd04600af699c5769beda5e92e5e417
Max Zhao <alcasa.mz@gmail.com>
2019-07-08 11:03:57 +0200

    More cleaning

--- images_postgis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DBNAME=geospatial
-inputdir="download-tif"
-
-for filename in $inputdir/*.tif; do
-	echo $filename
-	raster2pgsql -I -t 120x120 -C $filename | psql -U postgres -h home.arsbrevis.de -p 31313 -d $DBNAME >> postgis_import.log
-done
--- run_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-case $1 in
-build)
-	docker build -t sentinel-data .
-	;;
-*)
-	docker run --name sentinel-data -v $(pwd):/app -u $(id -u):$(id -g) --env COPERNICUS_USER=$COPERNICUS_USER --env COPERNICUS_PASS=$COPERNICUS_PASS --rm sentinel-data
-	;;
-esac
